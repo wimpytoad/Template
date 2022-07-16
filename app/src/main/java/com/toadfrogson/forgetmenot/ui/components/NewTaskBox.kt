bc56269e39f647610d72a08e5e3b0b040f8cbe55
aleksei <aleksei.terentev@electrolux.com>
2022-07-16 11:45:08 +0200

    Update libraries Add Data module Add Room DB library

---
+++ app/src/main/java/com/toadfrogson/forgetmenot/ui/components/NewTaskBox.kt
@@ -0,0 +1,11 @@
+package com.toadfrogson.forgetmenot.ui.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+
+@Composable
+fun NewTaskBox() {
+    Column {
+        Header(text = "What would you like to remember")
+    }
+}
